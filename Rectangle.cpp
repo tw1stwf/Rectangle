b110ab30a50425edaf657b2d008adfcb2a0d4812
Pasha <79657048+tw1stwf@users.noreply.github.com>
2021-11-23 20:49:16 +0300

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include "Header.h"
+
+using namespace std;
+using namespace N;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Rectangle ob;
+    ob.Input(5, 5, 10, 5, 10, 10, 5, 10);
+    int i = ob.RectanglePerimetr();
+
+    if (i < 50)
+    {
+        ob.get();
+        ob.Square();
+    }
+
+    else
+    {
+        cout << "Сумма сторон прямоугольника больше 50";
+    }
+
+    _getch();
+}
