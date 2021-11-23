d83b33860d7e6d13af88ffcaeca318ba9ee46574
Pasha <79657048+tw1stwf@users.noreply.github.com>
2021-11-23 22:50:05 +0300

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -8,7 +8,7 @@ namespace N
     {
     private:
         double x_A, y_A, x_B, y_B, x_C, y_C, x_D, y_D;
-        double  d1, d2, d3, d4, S, P, a, b;
+        double  d1, d2, d3, d4, P, a, b;
     public:
         void Input(double _x_A, double _y_A, double _x_B, double _y_B, double _x_C, double _y_C, double _x_D, double _y_D);
         void get();
