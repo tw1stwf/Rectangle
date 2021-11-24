12e609665d9cdbbf6ca3855809989a256a201d34
Pasha <79657048+tw1stwf@users.noreply.github.com>
2021-11-24 18:10:57 +0300

    Update Rectangle.h
    Added default values.

--- Rectangle.h
+++ Rectangle.h
@@ -8,11 +8,11 @@ namespace N
     {
     private:
         double x_A, y_A, x_B, y_B, x_C, y_C, x_D, y_D;
-        double  d1, d2, d3, d4, P, a, b;
+        double d1, d2, d3, d4, P;
     public:
-        void Input(double _x_A, double _y_A, double _x_B, double _y_B, double _x_C, double _y_C, double _x_D, double _y_D);
-        void get();
+        void Input(double _x_A = 5, double _y_A = 5, double _x_B = 10, double _y_B = 5, double _x_C = 10, double _y_C = 10, double _x_D = 5, double _y_D = 10);
+        void Print();
+        int isSquare();
         int RectanglePerimetr();
-        void Square();
     };
 }
